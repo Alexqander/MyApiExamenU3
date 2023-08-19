c8c3ca65c361d31eef50ab388920cde5edb0f861
Kevin Valdovinos <119825966+KevinValdovinos@users.noreply.github.com>
2023-08-18 20:11:07 -0600

    [DEVELOP] se agregó el ApplicationDbContext

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-
+using MyApi.Models;
 
 
 namespace MyApi
@@ -12,5 +12,26 @@ namespace MyApi
         {
 
         }
+       
+        public DbSet<Medicamento>? Medicamentos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            modelBuilder.Entity<Medicamento>().HasData(
+                new Medicamento()
+                {
+                    Id = 1,
+                    Nombre = "Paracetamol",
+                    Descripcion = "Analgésico y antipirético",
+                    DosisRecomendada = "2g de paracetamol",
+                    FormaAdministracion = "Solución oral",
+                    Indicaciones="Tomar cada 8 horas durante 10 dias."
+                }
+
+            );
+
+        }
+
     }
 }
