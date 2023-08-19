ad3d69bf7400604df44833717e5d0e682b624096
Kevin Valdovinos <119825966+KevinValdovinos@users.noreply.github.com>
2023-08-18 20:07:57 -0600

    [DEVELOP] se agregó MedicamentosController

---
+++ Controllers/MedicamentoController.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using Microsoft.EntityFrameworkCore;
+using MyApi.Models;
+namespace MyApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class MedicamentoController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context; //Esto es usado instead of this.context
+        public MedicamentoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("Index")] //Tipo de peticion
+        public async Task<IActionResult> Index()
+        { //Numero de argumentos que recibe
+            var listMedicamentos = await _context.Medicamentos.ToListAsync();
+            if (listMedicamentos == null || listMedicamentos.Count == 0)
+            {
+                return NoContent();
+            }
+            else
+            {
+                return Ok(listMedicamentos);
+            }
+        }
+        [HttpPost("Store")]
+        public async Task<HttpStatusCode> Store([FromBody] Medicamento medicamento)
+        {
+            if (medicamento == null)
+            {
+                return HttpStatusCode.BadRequest;
+            }
+            _context.Add(medicamento);
+            await _context.SaveChangesAsync();
+            return HttpStatusCode.Created;
+        }
+        [HttpGet("Show")]
+        public async Task<IActionResult> Show(int id)
+        {
+            var medicamento = await _context.Medicamentos.FindAsync(id);
+            if (medicamento == null)
+            {
+                return NotFound();
+            }
+            return Ok(medicamento);
+        }
+
+        [HttpDelete("Destroy")]
+        public async Task<IActionResult> Destroy(int id)
+        {
+            var medicamento = await _context.Medicamentos.FindAsync(id);
+            if (medicamento == null)
+            {
+                return NotFound();
+            }
+            _context.Medicamentos.Remove(medicamento);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+        [HttpPut("Update")]
+        public async Task<IActionResult> Update(int id, [FromBody] Medicamento medicamento)
+        {
+            if (medicamento == null || medicamento.Id != id)
+            {
+                return BadRequest();
+
+            }
+            var entity = await _context.Medicamentos.FindAsync(medicamento.Id);
+            if (entity == null)
+            {
+                return NotFound();//404
+
+
+            }
+            entity.Nombre = medicamento.Nombre;
+            entity.Descripcion = medicamento.Descripcion;
+            entity.DosisRecomendada = medicamento.DosisRecomendada;
+            entity.FormaAdministracion = medicamento.FormaAdministracion;
+             entity.Indicaciones = medicamento.Indicaciones;
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+
+
+
+    }
+}
