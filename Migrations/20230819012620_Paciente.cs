7b2430540bc128b4514d4ed0cc1b4ea4c9366831
Rodrigo Dorantes <106606020+rodrigoremen@users.noreply.github.com>
2023-08-18 20:12:57 -0600

    [DEVELOP] se agregaron las migraciones

---
+++ Migrations/20230819012620_Paciente.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Paciente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Paciente",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Especie = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Raza = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Peso = table.Column<int>(type: "int", nullable: true),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime(6)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Paciente", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Paciente",
+                columns: new[] { "Id", "Especie", "FechaNacimiento", "Nombre", "Peso", "Raza" },
+                values: new object[] { 1, " Perro", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Guarumo", 12, "French Poodle" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Paciente");
+        }
+    }
+}
